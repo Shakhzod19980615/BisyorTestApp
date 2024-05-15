d0e9097e0abef5333c486f7d92a4891a701ca328
shakhzod_bakhromov <shakhzod_bakhromov@mail.ru>
2024-05-15 11:55:26 +0100

    signUpViewModel done

--- app/src/main/java/com/example/testapp/presentation/authoration/registration/FragmentRegistration.kt
+++ app/src/main/java/com/example/testapp/presentation/authoration/registration/FragmentRegistration.kt
@@ -1,7 +1,36 @@
 package com.example.testapp.presentation.authoration.registration
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.testapp.R
+import com.example.testapp.databinding.WindowRegistrationBinding
+import com.example.testapp.presentation.home.viewModel.authoration.SignUpViewModel
+import kotlin.properties.Delegates
 
 class FragmentRegistration: Fragment(R.layout.window_registration){
+    private  val signUpViewModel: SignUpViewModel by viewModels()
+    private var binding : WindowRegistrationBinding by Delegates.notNull()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = WindowRegistrationBinding.inflate(inflater)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.submitButton.setOnClickListener {
+            val login = binding.editTextLogin.text.toString()
+            val password = binding.editTextPassword.text.toString()
+            signUpViewModel.signUp(login, password)
+        }
+    }
 }
--- app/src/main/java/com/example/testapp/presentation/home/viewModel/authoration/SignUpViewModel.kt
+++ app/src/main/java/com/example/testapp/presentation/home/viewModel/authoration/SignUpViewModel.kt
@@ -8,10 +8,12 @@ import androidx.lifecycle.viewModelScope
 import com.example.testapp.common.Resource
 import com.example.testapp.data.request.RegistrationRequest
 import com.example.testapp.domain.model.basicResponseModel.BasicResponseModel
+import com.example.testapp.domain.use_case.authoration.PhoneNumberValidationUseCase
 import com.example.testapp.domain.use_case.authoration.SignUpUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.io.IOException
@@ -19,11 +21,22 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SignUpViewModel @Inject constructor(
-       private val signUpUseCase: SignUpUseCase
+       private val signUpUseCase: SignUpUseCase,
+        private val phoneNumberValidationUseCase: PhoneNumberValidationUseCase
 ): ViewModel(){
     private val _signUp = MutableStateFlow<Resource<BasicResponseModel>>(Resource.Loading())
+    private val _phoneNumberValidation = MutableStateFlow<Boolean?>(null)
+    val phoneNumberValidation = _phoneNumberValidation.asStateFlow()
+
     @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
-    fun signUp(request: RegistrationRequest){
+    fun signUp(login: String, password: String) {
+        val isPhoneNumberValid = phoneNumberValidationUseCase.isPhoneNumberValid(login)
+        if (!isPhoneNumberValid) {
+            // Communicate validation result to the view
+            _phoneNumberValidation.value = false
+            return
+        }
+        val request = RegistrationRequest(login, password)
         viewModelScope.launch {
             withContext(Dispatchers.IO){
                 try {
